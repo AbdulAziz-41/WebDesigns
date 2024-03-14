d5199517a571064d75b6baa59e8f3bdf7bd83a02
@AbdulAziz471 <aziz.tariq@tetralogicx.com>
2024-03-14 10:34:42 +0500

    Changes

--- src/Components/Bannars/HowitsWorkPageBannars/TextWithImageHIsWBannar.jsx
+++ src/Components/Bannars/HowitsWorkPageBannars/TextWithImageHIsWBannar.jsx
@@ -8,16 +8,18 @@ export default function TextWithImageHIsWBannar({
   bannarSpan,
   bannar_subheadingClass,
   bannar_subheading,
+  bannar_subheading_2,
+  Content,
 }) {
   return (
     <div className="bg-[#37513C]  ">
       <div
-        className={` text-white gap-5 max-w-[1440px] mx-auto flex items-center justify-center pt-[132px]  ${bannarformation_Class}`}
+        className={` text-white gap-5  max-w-[1440px] mx-auto flex items-center justify-center pt-[132px]  ${bannarformation_Class}`}
       >
         <div className="73%">
           <img src={bannarImage} alt="" />
         </div>
-        <div className="">
+        <div className={`${Content}`}>
           <h1
             className={` max-w-[500px] w-fullz text-white  text-[30px] sm:text-[45px] md:text-[60px] lg:text-[72px] lg:leading-[66.96px] tracking-[-0.07em] pb-10
  font-light ${bannar_heading_Class}`}
@@ -34,6 +36,12 @@ export default function TextWithImageHIsWBannar({
             className={` text-[12px] sm:text-[14px] md:text-[17px] lg:text-[21px]  tracking-[ -0.05em] leading-[20.7px] sm:leading-[25px] md:leading-[30px] lg:leading-[35.7px]  ${bannar_subheadingClass}`}
           >
             {bannar_subheading}
+          </p>
+          <br />
+          <p
+            className={` text-[12px] sm:text-[14px] md:text-[17px] lg:text-[21px]  tracking-[ -0.05em] leading-[20.7px] sm:leading-[25px] md:leading-[30px] lg:leading-[35.7px]  ${bannar_subheadingClass}`}
+          >
+            {bannar_subheading_2}
           </p>
         </div>
       </div>
--- src/Pages/HowItsWorkPage/HowItsWorkPage.jsx
+++ src/Pages/HowItsWorkPage/HowItsWorkPage.jsx
@@ -4,6 +4,7 @@ import HowItsWorkDark from "../../Components/HowItsWork/HowitsWorkDark";
 import TopBannar from "../../Components/Bannars/HowitsWorkPageBannars/TopBannar";
 
 import mobileview from "../../assets/images/mobileview.png";
+import graph from "../../assets/images/graph.png";
 import TextWithImageHIsWBannar from "../../Components/Bannars/HowitsWorkPageBannars/TextWithImageHIsWBannar";
 export default function HowItsWorkPage() {
   return (
@@ -16,25 +17,26 @@ export default function HowItsWorkPage() {
         bannar_subheadingClass=""
         bannarImage={mobileview}
         bannar_heading_Class=""
+        Content=""
         bannar_heading="Leveraging the power of the "
         bannar_subheading="Since inception, we’ve offered over $700mm in blue chip contemporary art investments, exemplifying not only the success of our strategy, but the favorable performance of this asset class."
       />
       <HowItsWork bg_color="bg-[#EFEFEF] " text_color={"text-[#272727]"} />
-      <TextWithImageHIsWBannar
-        bannarSpanclass=""
-        bannarSpan="Process"
-        bannarformation_Class=""
-        bannar_subheadingClass=""
-        bannarImage={mobileview}
-        bannar_heading_Class=""
-        bannar_heading="Investment 
-        "
-        bannar_subheading="Our company focuses on acquiring movies by contemporary filmmakers who exhibit attractive investment characteristics. The filmmakers are identified by the research team using data analytics built on top of its proprietary database.
-        Works by those filmmakers are then sourced via our acquisitions team, a group with decades of art market transaction and diligence experience. Our company maintains a buy and hold approach, selling opportunistically in order to increase returns for investors."
-      />
       <EmailRequestAndBtns
         bg_color="bg-[#EFEFEF]  "
         btnColor={"border-[#498856]"}
+      />
+      <TextWithImageHIsWBannar
+        bannarSpanclass=""
+        bannarSpan="Process"
+        bannarformation_Class="flex-row-reverse gap-[47px] !items-start !pt-[141px] !pb-[87px]"
+        bannar_subheadingClass=""
+        bannarImage={graph}
+        bannar_heading_Class=""
+        bannar_heading="Investment"
+        Content="!w-[68%]"
+        bannar_subheading="Our company focuses on acquiring movies by contemporary filmmakers who exhibit attractive investment characteristics. The filmmakers are identified by the research team using data analytics built on top of its proprietary database."
+        bannar_subheading_2="Works by those filmmakers are then sourced via our acquisitions team, a group with decades of art market transaction and diligence experience. Our company maintains a buy and hold approach, selling opportunistically in order to increase returns for investors."
       />
     </>
   );
